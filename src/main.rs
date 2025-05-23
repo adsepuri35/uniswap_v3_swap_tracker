472bc61e0b64559701568f9254ba3a40badbf30b
Advait Sepuri <advait.sepuri@alchemy.com>
2025-05-23 11:58:27 -0400

    increment swaps for pools

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::io::{self, Write};
-use alloy::{network::Ethereum, providers::{Provider, ProviderBuilder}, rpc::types::{Filter, Topic}, sol_types::SolEvent, core::primitives::Address};
+use alloy::{core::primitives::Address, network::Ethereum, providers::{self, Provider, ProviderBuilder}, rpc::types::{Filter, Topic}, sol_types::SolEvent};
 use anyhow::Result;
 use dotenv::dotenv;
 use std::{env, collections::HashMap};
 use amms::amms::uniswap_v3::{IUniswapV3Factory::IUniswapV3FactoryInstance, IUniswapV3Pool::IUniswapV3PoolInstance, IUniswapV3PoolEvents::Swap};
 // use reqwest::Client;
 
+
+// other file imports
 mod ierc20;
 use ierc20::IERC20;
+mod poollnfo;
+use poollnfo::PoolInfo;
 
 
 const BLOCKS_TO_TRACK: u64 = 0;
@@ -19,6 +23,8 @@ const BLOCKS_TO_TRACK: u64 = 0;
 //     "0xCBCdF9626bC03E24f779434178A73a0B4bad62eD", // WBTC-ETH 0.3%
 //     "0x4e68Ccd3E89f51C3074ca5072bbAC773960dFa36"  // USDT-ETH 0.3%
 // ];
+
+// fn sqrt_price_x96_to_price(sqrt_price_x96: uint160) -> f64 {
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -71,52 +77,79 @@ async fn main() -> Result<()> {
 
     // println!("{:?}", logs);
     println!("Num Uniswap swap events: {}", logs.len());
-    // if logs.len() > 0 {
-    //     println!("{:#?}", logs[0])
-    // }
+    if logs.len() > 0 {
+        println!("{:#?}", logs[0])
+    }
 
-    let mut address_to_symbol: HashMap<Address, String> = HashMap::new();
+    let mut token_address_to_symbol: HashMap<Address, String> = HashMap::new();
+    let mut pool_address_to_index: HashMap<Address, u16> = HashMap::new();
+    let mut pool_storage: Vec<PoolInfo> = Vec::new();
+
 
     for log in logs {
         if let Ok(decode) =  log.log_decode::<Swap>() {
 
             let swap = decode.data();
-            println!("Pool address: {:?}", log.address());
+            let pool_address = log.address();
 
-            let contract = IUniswapV3PoolInstance::new(log.address(), provider.clone());
+
+            let contract = IUniswapV3PoolInstance::new(pool_address, provider.clone());
+
+            // acquire token addresses + symbols
             let token0_address = contract.token0().call().await?;
             let token1_address = contract.token1().call().await?;
-
-
-            if !address_to_symbol.contains_key(&token0_address) {
+            if !token_address_to_symbol.contains_key(&token0_address) {
                 let ierc20_token0 = IERC20::new(token0_address, provider.clone());
                 let token0_symbol = ierc20_token0.symbol().call().await?;
-                println!("Stored: {:?}", token0_symbol);
-                address_to_symbol.insert(token0_address, token0_symbol);
-            } else {
-                println!("Repeat of: {:?}", address_to_symbol.get(&token0_address).unwrap_or(&"Unknown".to_string()));
+                token_address_to_symbol.insert(token0_address, token0_symbol);
             }
-            if !address_to_symbol.contains_key(&token1_address) {
+            if !token_address_to_symbol.contains_key(&token1_address) {
                 let ierc20_token1 = IERC20::new(token1_address, provider.clone());
                 let token1_symbol = ierc20_token1.symbol().call().await?;
-                println!("Stored: {:?}", token1_symbol);
-                address_to_symbol.insert(token1_address, token1_symbol);
-            } else {
-                println!("Repeat of: {:?}", address_to_symbol.get(&token1_address).unwrap_or(&"Unknown".to_string()));
+                token_address_to_symbol.insert(token1_address, token1_symbol);
             }
-            
 
-            // let ierc20_token0 = IERC20::new(token0_address, provider.clone());
-            // let ierc20_token1 = IERC20::new(token1_address, provider.clone());
-            // println!("Token 0: {:?}", ierc20_token0.symbol().call().await?);
-            // println!("Token 1: {:?}", ierc20_token1.symbol().call().await?);
 
-    
-            println!("Sender: {:?}", swap.sender);
-            println!("Recipient: {:?}", swap.recipient);
+
+            // add structs to pool storage (new pool)
+            if !pool_address_to_index.contains_key(&pool_address) {
+                pool_address_to_index.insert(pool_address, pool_storage.len() as u16);
+
+                let token0_symbol = token_address_to_symbol
+                    .get(&token0_address)
+                    .cloned()
+                    .unwrap_or_else(|| String::from("Unknown"));
+                    
+                let token1_symbol = token_address_to_symbol
+                    .get(&token1_address)
+                    .cloned()
+                    .unwrap_or_else(|| String::from("Unknown"));
+
+                let new_pool = PoolInfo::new(pool_address, token0_address, token1_address, token0_symbol, token1_symbol, 1);
+                pool_storage.push(new_pool);
+            } else {
+                // implement functionality for pool updates
+                if let Some(&index) = pool_address_to_index.get(&pool_address) {
+                    let pool = &mut pool_storage[index as usize];
+                    
+                    // Update the pool with the new swap information
+                    pool.increment_swap_count();
+                } else {
+                    println!("Error: Pool found in HashMap but no index available");
+                }
+
+            }
+            // println!("Sender: {:?}", swap.sender);
+            // println!("Recipient: {:?}", swap.recipient);
 
         }
     }
+
+
+    for pool in &pool_storage {
+        println!("{:?} = {:?}" ,pool.get_pool_name(), pool.get_swap_count());
+    }
+
 
     Ok(())
 
---
+++ src/poollnfo.rs
@@ -0,0 +1,62 @@
+use std::ops::Add;
+
+use alloy::core::primitives::Address;
+
+
+#[derive(Debug)]
+pub struct PoolInfo {
+    pool_address: Address,
+    pool_name: String,
+    token0: Address,
+    token1: Address,
+    token0_symbol: String,
+    token1_symbol: String,
+    swaps_tracked: usize,
+
+    // // needed
+    // fee: u32,
+    // tick_spacing: i32,
+    // swaps_tracked: usize,        // number of swaps seen
+    // last_price: Option<f64>,     // derived from sqrtPriceX96
+}
+
+impl PoolInfo {
+    pub fn new(
+        pool_address: Address,
+        token0: Address,
+        token1: Address,
+        token0_symbol: String,
+        token1_symbol: String,
+        swaps_tracked: usize,
+    ) -> Self {
+        let pool_name = if token0_symbol.is_empty() || token1_symbol.is_empty() {
+            format!("Pool-{:?}", pool_address)
+        } else if token0_symbol < token1_symbol {
+            format!("{}/{}", token0_symbol, token1_symbol)
+        } else {
+            format!("{}/{}", token1_symbol, token0_symbol)
+        };
+
+        PoolInfo {
+            pool_address,
+            pool_name,
+            token0,
+            token1,
+            token0_symbol,
+            token1_symbol,
+            swaps_tracked,
+        }
+    }
+
+    pub fn increment_swap_count(&mut self) {
+        self.swaps_tracked += 1;
+    }
+
+    pub fn get_swap_count(&self) -> &usize {
+        &self.swaps_tracked
+    }
+
+    pub fn get_pool_name(&self) -> &str {
+        &self.pool_name
+    }
+}
